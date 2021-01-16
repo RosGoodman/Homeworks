498267c4a322fea292bb523124d5d147fa7e8af9
Pavel <pavel.official@bk.ru>
2021-01-16 22:51:59 +0300

    Homework-1 done

---
+++ Lesson-1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как тебя зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToString("D")}.");
+        }
+    }
+}
