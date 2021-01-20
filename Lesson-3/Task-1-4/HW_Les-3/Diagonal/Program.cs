9b76ec9126589188e07a12392085740f9e724013
Ros <pavel.official@bk.ru>
2021-01-20 18:46:16 +0300

    Task-1 done

--- Lesson-3/Task-1-4/HW_Les-3/Diagonal/Program.cs
+++ Lesson-3/Task-1-4/HW_Les-3/Diagonal/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Diagonal
 {
@@ -10,6 +6,48 @@ namespace Diagonal
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размер массива:");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Program program = new Program();
+            int[,] rndArr = program.RandomArray(lenght);
+
+            program.PrintDiagonal(rndArr);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>Печать диагонали массива.</summary>
+        /// <param name="rndArr">Массив значений.</param>
+        private void PrintDiagonal(int[,] rndArr)
+        {
+            Console.WriteLine();
+            for (int i=0; i < rndArr.GetLength(0); i++)
+            {
+                Console.Write(rndArr[i,i] + " ");
+            }
+        }
+
+        /// <summary>Получение двумерного массива установленного размера и с рандомными значениями.</summary>
+        /// <param name="lenght">Длина массива.</param>
+        /// <param name="width">Ширина массива.</param>
+        /// <returns>Двумерный массив.</returns>
+        private int[,] RandomArray(int lenght)
+        {
+            Random random = new Random();
+            int[,] rndArr = new int[lenght, lenght];
+            for(int i = 0; i<lenght; i++)
+            {
+                for(int j = 0; j< lenght; j++)
+                {
+                    rndArr[i, j] = random.Next(0, 100);
+                    Console.Write($"{rndArr[i,j]} ");    //вывод для наглядности
+                    if (rndArr[i, j] < 10) Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            return rndArr;
         }
     }
 }
