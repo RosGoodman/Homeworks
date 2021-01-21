9f82405cfc6594c8fd56f6e2948f992ed0b84dd4
Pavel <pavel.official@bk.ru>
2021-01-21 18:43:19 +0300

    add: commands, printAllComm, arrayContacts, printContacts

--- Lesson-3/Task-1-4/HW_Les-3/Task_2/PhoneBook.cs
+++ Lesson-3/Task-1-4/HW_Les-3/Task_2/PhoneBook.cs
@@ -6,41 +6,107 @@ namespace Task_2
     {
         static void Main()
         {
-            Console.WriteLine("Введите \"com\", что-бы увидеть возможные команды.");
-            string command = Console.ReadLine();
+            PhoneBook phoneBook = new PhoneBook();
+            string[,] contacts = phoneBook.GetArrayContacts();
+
+            string command;
+            do
+            {
+                Console.WriteLine("Введите \"com\", что-бы увидеть возможные команды.");
+                command = (Console.ReadLine().ToLower());
+                phoneBook.Commands(command, contacts);
+            } while (command != "q");
+            
         }
 
-        /// <summary></summary>
-        /// <param name="command"></param>
-        private void Commands(string command)
+        /// <summary>Получить массив контактов.</summary>
+        /// <returns>Массив контактов (имя, тел.\email)</returns>
+        private string[,] GetArrayContacts()
         {
-            switch (command)
+            string[,] contacts = new string[5,2];
+
+            contacts[0, 0] = "Иван";
+            contacts[0, 1] = "278-32-66";
+            contacts[1, 0] = "Елена";
+            contacts[1, 1] = "elena22@gmail.com";
+            contacts[2, 0] = "Анатолий";
+            contacts[2, 1] = "(911) 256 77 77";
+
+            return contacts;
+        }
+
+        /// <summary>Определение и запуск команды.</summary>
+        /// <param name="command">Команда.</param>
+        /// <param name="contacts">Массив конактов.</param>
+        private void Commands(string command, string[,] contacts)
+        {
+            string[] comParts = command.Split(' ');
+            int contactNumb = 0;
+            if (comParts.Length == 2)
+            {
+                contactNumb = Convert.ToInt32(comParts[1]);
+                if (contactNumb > contacts.GetLength(0) || contactNumb < 0) PrintErrore("wrongContNumb");
+            }
+
+            switch (comParts[0])
             {
                 case "com":
                     PrintAllCommand();
+                    Console.WriteLine("1");
                     break;
                 case "list":
-                    PrintAllCommand();
+                    Console.WriteLine("2");
+                    ShowContactList(contacts, contactNumb);
                     break;
                 case "add":
-                    PrintAllCommand();
+                    Console.WriteLine("3");
                     break;
-                case "del(1)":
-                    PrintAllCommand();
+                case "del":
+                    Console.WriteLine("4");
                     break;
-                case "change(1)":
-                    PrintAllCommand();
+                case "change":
+                    Console.WriteLine("5");
                     break;
             }
         }
 
+        /// <summary>Вывод контактов в консоль.</summary>
+        /// <param name="contacts">Массив контактов.</param>
+        /// <param name="contactNumb">Номер выводимого контакта.</param>
+        private void ShowContactList(string[,] contacts, int contactNumb)
+        {
+            int x = contacts.GetLength(0);
+            if (contactNumb == contacts.GetLength(0))
+            {
+                for(int i = 0; i<contacts.GetLength(0); i++)
+                {
+                    if(contacts[i,0] != "" && contacts[i,0] != null)
+                        Console.WriteLine("{0}  - {1}", contacts[i,0], contacts[i,1]);
+                }
+            }
+            else Console.WriteLine("{0}  - {1}", contacts[contactNumb,0], contacts[contactNumb,1]);
+        }
+
+        /// <summary>Вывод списка команд.</summary>
         private void PrintAllCommand()
         {
-            Console.WriteLine($"com - вывести все команды" +
-                $"list - вывести весь список контактов" +
-                $"add - добавить контакт (через пробел \"имя телефон/email\")" +
-                $"del(1) - удалить контакт под указанным номером" +
-                $"change(1) - изменить контакт под указанным номером");
+            Console.WriteLine($"\ncom       - вывести все команды\n" +
+                $"show  1   - вывести весь список контактов\n" +
+                $"add       - добавить контакт (через пробел \"имя телефон/email\")\n" +
+                $"del  1    - удалить контакт под указанным номером\n" +
+                $"change  1 - изменить контакт под указанным номером\n" +
+                $"q         - выход\n" +
+                $"Номер контакта не оьязателен, без них команда воздействует на все контакты.\n");
+        }
+
+        private void PrintErrore(string errName)
+        {
+            switch (errName)
+            {
+                case "wrongContNumb":
+                    Console.WriteLine("Неверное значение номера контакта.");
+                    break;
+            }
         }
     }
 }
