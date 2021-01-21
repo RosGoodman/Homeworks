2951431cdb8465eb2f75bb30694031b11c5b2500
Ros Goodman <pavel.official@bk.ru>
2021-01-21 13:13:09 +0300

    change task-2

---
+++ Lesson-3/Task-1-4/HW_Les-3/Task_2/PhoneBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_2
+{
+    class PhoneBook
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите \"com\", что-бы увидеть возможные команды.");
+            string command = Console.ReadLine();
+        }
+
+        /// <summary></summary>
+        /// <param name="command"></param>
+        private void Commands(string command)
+        {
+            switch (command)
+            {
+                case "com":
+                    PrintAllCommand();
+                    break;
+                case "list":
+                    PrintAllCommand();
+                    break;
+                case "add":
+                    PrintAllCommand();
+                    break;
+                case "del(1)":
+                    PrintAllCommand();
+                    break;
+                case "change(1)":
+                    PrintAllCommand();
+                    break;
+            }
+        }
+
+        private void PrintAllCommand()
+        {
+            Console.WriteLine($"com - вывести все команды" +
+                $"list - вывести весь список контактов" +
+                $"add - добавить контакт (через пробел \"имя телефон/email\")" +
+                $"del(1) - удалить контакт под указанным номером" +
+                $"change(1) - изменить контакт под указанным номером");
+        }
+    }
+}
