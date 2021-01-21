2b720f7246bc489321369707da04dee54bad7a25
Pavel <pavel.official@bk.ru>
2021-01-21 20:53:59 +0300

    method AddContacts

--- Lesson-3/Task-1-4/HW_Les-3/Task_2/PhoneBook.cs
+++ Lesson-3/Task-1-4/HW_Les-3/Task_2/PhoneBook.cs
@@ -14,6 +14,7 @@ namespace Task_2
             {
                 Console.WriteLine("Введите \"com\", что-бы увидеть возможные команды.");
                 command = (Console.ReadLine().ToLower());
+                Console.WriteLine();
                 phoneBook.Commands(command, contacts);
             } while (command != "q");
             
@@ -41,25 +42,20 @@ namespace Task_2
         private void Commands(string command, string[,] contacts)
         {
             string[] comParts = command.Split(' ');
-            int contactNumb = 0;
-            if (comParts.Length == 2)
-            {
-                contactNumb = Convert.ToInt32(comParts[1]);
-                if (contactNumb > contacts.GetLength(0) || contactNumb < 0) PrintErrore("wrongContNumb");
-            }
 
             switch (comParts[0])
             {
                 case "com":
                     PrintAllCommand();
-                    Console.WriteLine("1");
+                    Console.WriteLine("1");     //TODO: удалить выводы.
                     break;
                 case "list":
                     Console.WriteLine("2");
-                    ShowContactList(contacts, contactNumb);
+                    ShowContactList(contacts, comParts);
                     break;
                 case "add":
                     Console.WriteLine("3");
+                    AddContact(contacts, comParts);
                     break;
                 case "del":
                     Console.WriteLine("4");
@@ -67,16 +63,62 @@ namespace Task_2
                 case "change":
                     Console.WriteLine("5");
                     break;
+                case "":
+                    Console.WriteLine("0");
+                    break;
+                default:
+                    PrintErrors("wrongCommand");
+                    break;
             }
         }
 
+        #region GetContactCount
+        /// <summary>Получение кол-ва контактов.</summary>
+        /// <param name="contacts">Массив контактов.</param>
+        /// <param name="comParts">Команда, разбитая на составные.</param>
+        /// <returns>Кол-во контактов.</returns>
+        private int GetContactCount(string[,] contacts, string[] comParts)
+        {
+            int contactNumb = 1;
+            if (comParts.Length > 1)
+            {
+                contactNumb = Convert.ToInt32(comParts[1]);
+                if (contactNumb > contacts.GetLength(0) || contactNumb < 1) PrintErrors("wrongContNumb");
+            }
+            return contactNumb;
+        }
+        #endregion
+
+        #region AddContacts
+        private void AddContact(string[,] contacts, string[] comParts)
+        {
+            int contactsCount = 0;
+            for(int i=0; i < contacts.GetLength(0); i++)
+            {
+                if(contacts[i, 0] != null) contactsCount++;
+            }
+            int x = contacts.GetLength(0);
+            if (contacts.GetLength(0) == contactsCount)
+            {
+                PrintErrors("outOfRange");
+                return;
+            }
+            else
+            {
+                contacts[contactsCount, 0] = comParts[1];   //имя
+                if (comParts.Length == 3) contacts[contactsCount, 1] = comParts[2];   //номер/email
+            }
+        }
+        #endregion
+
+        #region ShowContactList
         /// <summary>Вывод контактов в консоль.</summary>
         /// <param name="contacts">Массив контактов.</param>
         /// <param name="contactNumb">Номер выводимого контакта.</param>
-        private void ShowContactList(string[,] contacts, int contactNumb)
+        private void ShowContactList(string[,] contacts, string[] comParts)
         {
-            int x = contacts.GetLength(0);
-            if (contactNumb == contacts.GetLength(0))
+            int contactNumb = GetContactCount(contacts, comParts);
+            if (comParts.Length < 2)
             {
                 for(int i = 0; i<contacts.GetLength(0); i++)
                 {
@@ -84,13 +126,18 @@ namespace Task_2
                         Console.WriteLine("{0}  - {1}", contacts[i,0], contacts[i,1]);
                 }
             }
-            else Console.WriteLine("{0}  - {1}", contacts[contactNumb,0], contacts[contactNumb,1]);
+            else if (contacts[contactNumb-1, 0] != null)
+                Console.WriteLine("{0}  - {1}", contacts[contactNumb-1,0], contacts[contactNumb-1,1]);
+            else
+                PrintErrors("wrongContNumb");
         }
+        #endregion
 
+        #region PrintCommands
         /// <summary>Вывод списка команд.</summary>
         private void PrintAllCommand()
         {
-            Console.WriteLine($"\ncom       - вывести все команды\n" +
+            Console.WriteLine($"com       - вывести все команды\n" +
                 $"show  1   - вывести весь список контактов\n" +
                 $"add       - добавить контакт (через пробел \"имя телефон/email\")\n" +
                 $"del  1    - удалить контакт под указанным номером\n" +
@@ -98,15 +145,27 @@ namespace Task_2
                 $"q         - выход\n" +
                 $"Номер контакта не оьязателен, без них команда воздействует на все контакты.\n");
         }
+        #endregion
 
-        private void PrintErrore(string errName)
+        #region Errors
+        /// <summary>Обработка ошибок.</summary>
+        /// <param name="errName">Имя ошибки.</param>
+        private void PrintErrors(string errName)
         {
             switch (errName)
             {
                 case "wrongContNumb":
-                    Console.WriteLine("Неверное значение номера контакта.");
+                    Console.WriteLine("ERR: Неверное значение номера контакта.");
+                    break;
+                case "wrongCommand":
+                    Console.WriteLine("ERR: Нусуществующая команда.");
+                    break;
+                case "outOfRange":
+                    Console.WriteLine("ERR: В книге нет места.");
                     break;
             }
+            Main();
         }
+        #endregion
     }
 }
