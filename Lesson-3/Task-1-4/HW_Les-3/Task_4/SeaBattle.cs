b8d891e744d017fa005d98e8bcd29e863272a3fb
Pavel <pavel.official@bk.ru>
2021-01-23 01:47:49 +0300

    ненужный коммит

---
+++ Lesson-3/Task-1-4/HW_Les-3/Task_4/SeaBattle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class SeaBattle
+    {
+        static void Main()
+        {
+            Dictionary<string, int> ships = new Dictionary<string, int>();
+            ships.Add("four", 1);
+            ships.Add("three", 2);
+            ships.Add("two", 3);
+            ships.Add("one", 4);
+
+            foreach(var ship in ships.Keys)
+            {
+                Random random = new Random(2);
+                int dir = random.Next(0, 100);
+                Console.WriteLine(dir);
+                await Task.Delay()
+            }
+        }
+    }
+}
