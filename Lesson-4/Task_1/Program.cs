29c26acef52cfa0f2f6e0aab56b2959dfb85960d
Pavel <pavel.official@bk.ru>
2021-01-25 07:14:22 +0300

    Less-4 Task_1 done

---
+++ Lesson-4/Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            string firstName = "Петр";
+            string lastName = "Иванов";
+            string patronymic = "Васильевич";
+            Console.WriteLine(program.GetFullName(firstName, lastName, patronymic));
+
+            Console.WriteLine(program.GetFullName("Василий", "Петров", "Николаевич"));
+
+            lastName = "Васильев";
+            string fullName = program.GetFullName("Иван", lastName, "Федорович");
+            Console.WriteLine(fullName);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>Получить ФИО строкой.</summary>
+        /// <param name="firstName">Имя.</param>
+        /// <param name="lastName">Фамилия.</param>
+        /// <param name="patronymic">Отчество.</param>
+        /// <returns>ФИО.</returns>
+        private string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return lastName + " " + firstName + " " + patronymic;
+        }
+    }
+}
