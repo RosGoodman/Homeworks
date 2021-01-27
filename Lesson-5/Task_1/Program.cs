760ae217c3a4c92fd5b92d96e713a01ed6f7726d
Ros <pavel.official@bk.ru>
2021-01-27 17:24:48 +0300

    Task-1 done

---
+++ Lesson-5/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = UserInput();
+            WriteTextInFile(str);
+            Console.WriteLine("Готово!");
+            Console.ReadLine();
+        }
+
+        /// <summary>Ввод в консоль.</summary>
+        /// <returns>Полученная строка.</returns>
+        private static string UserInput()
+        {
+            Console.WriteLine("Введите строку, которая будет записана в файл.");
+            return Console.ReadLine();
+        }
+
+        /// <summary>Запись в txt файл.</summary>
+        /// <param name="str">Записываемая строка.</param>
+        private static void WriteTextInFile(string str)
+        {
+            string filename = "taxt.txt";
+            File.WriteAllText(filename, str);
+        }
+    }
+}
