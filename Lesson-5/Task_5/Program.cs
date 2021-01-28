60bcdf1312ec23b03e85c0429a569c48a86e6e9f
Ros <pavel.official@bk.ru>
2021-01-28 19:12:48 +0300

    add Task_5

---
+++ Lesson-5/Task_5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        private string _addresFolder;
+        static void Main(string[] args)
+        {
+            ToDo task = new ToDo();
+            SaveTasksJson(task);
+
+            List<ToDo> tasks = new List<ToDo>();
+            LoadTasksJson(tasks);
+        }
+
+        private static void LoadTasksJson(List<ToDo> tasks)
+        {
+            string json = File.ReadAllText("tasks.json");
+            ToDo task = JsonSerializer.Deserialize<ToDo>(json);
+        }
+
+        private static void LoadTasksXml(List<ToDo> tasks)
+        {
+
+        }
+
+        private static void LoadTasksBin(List<ToDo> tasks)
+        {
+
+        }
+
+        private static void SaveTasksJson(ToDo task)
+        {
+            string json = JsonSerializer.Serialize(task);
+            File.WriteAllText("tasks.json", json);
+        }
+
+        private static void SaveTasksXml()
+        {
+
+        }
+
+        private static void SaveTasksBin()
+        {
+
+        }
+    }
+}
---
+++ Lesson-5/Task_5/ToDo.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Task_5
+{
+    internal class ToDo
+    {
+        private string _title = "задача";
+        private bool _isDone = true;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+    }
+}
