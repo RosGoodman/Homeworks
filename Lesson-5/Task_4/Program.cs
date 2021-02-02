2e0591caaf427e774da9724398d29c8f1baf9f02
Ros <pavel.official@bk.ru>
2021-02-02 18:57:57 +0300

    task_4

---
+++ Lesson-5/Task_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task_4
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            string directory = string.Empty;
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+                directory = FBD.SelectedPath;
+
+            // Перечень всех файлов и папок, вложенных в workDir
+            string[] entries = Directory.GetFileSystemEntries(directory, "*", SearchOption.AllDirectories);
+
+            int spaces = entries[0].Split('\\').Length;
+            string space = "";
+            var parentPath = Directory.GetParent(entries[0]);
+            Console.WriteLine(Path.GetFileName(parentPath.Name));
+            for (int i = 1; i < entries.Length; i++)
+            {
+                var thisParent = Directory.GetParent(entries[i]);
+                if (thisParent.Name != parentPath.Name)
+                {
+                    space = string.Empty;
+                    for (int j = 0; j < (parentPath.FullName).Split('\\').Length - spaces; j++)
+                    {
+                        space += "    ";
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(space + Path.GetFileName(thisParent.FullName));
+                    Console.ResetColor();
+                    parentPath = thisParent;
+
+                    space += "    ";
+                }
+
+                Console.WriteLine(space + Path.GetFileName(entries[i]));
+            }
+            Console.ReadLine();
+        }
+    }
+}
