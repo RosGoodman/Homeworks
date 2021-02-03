31cd91a5f904785bc1043a4ca0ff3b6de33f5938
Ros Goodman <pavel.official@bk.ru>
2021-02-03 13:24:26 +0300

    метод получение дерева каталогов циклом

--- Lesson-5/Task_4/Program.cs
+++ Lesson-5/Task_4/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
 
@@ -7,44 +6,71 @@ namespace Task_4
 {
     class Program
     {
-
         [STAThread]
         static void Main()
         {
-            string directory = string.Empty;
-            FolderBrowserDialog FBD = new FolderBrowserDialog();
-            if (FBD.ShowDialog() == DialogResult.OK)
-                directory = FBD.SelectedPath;
+            Console.WriteLine("Введите путь к необходимой директории, для открытия диалога выбора директории просто нажмите enter.");
 
-            // Перечень всех файлов и папок, вложенных в workDir
+            string directory = Console.ReadLine();
+            if(directory == "")
+            {
+                FolderBrowserDialog FBD = new FolderBrowserDialog();
+                if (FBD.ShowDialog() == DialogResult.OK)
+                    directory = FBD.SelectedPath;
+            }
+
             string[] entries = Directory.GetFileSystemEntries(directory, "*", SearchOption.AllDirectories);
+            //Cycle(entries);
+            Recursive(directory);
 
-            int spaces = entries[0].Split('\\').Length;
+            Console.WriteLine("Список файлов, полученный циклом записан в файл CycleTree.txt");
+            Console.ReadLine();
+        }
+
+        #region Цикл
+
+        /// <summary>Получить дерево каталогов с помощью цикла.</summary>
+        /// <param name="entries">Список всех путей к вложенным файлам.</param>
+        private static void Cycle(string[] entries)
+        {
+            string[] files = new string[entries.Length * 2];
+            int directoryDepth = entries[0].Split('\\').Length;
             string space = "";
             var parentPath = Directory.GetParent(entries[0]);
-            Console.WriteLine(Path.GetFileName(parentPath.Name));
+            files[0] = Path.GetFileName(parentPath.Name);
+
             for (int i = 1; i < entries.Length; i++)
             {
                 var thisParent = Directory.GetParent(entries[i]);
                 if (thisParent.Name != parentPath.Name)
                 {
                     space = string.Empty;
-                    for (int j = 0; j < (parentPath.FullName).Split('\\').Length - spaces; j++)
+                    for (int j = 0; j < (thisParent.FullName).Split('\\').Length - directoryDepth; j++)
                     {
                         space += "    ";
                     }
 
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(space + Path.GetFileName(thisParent.FullName));
-                    Console.ResetColor();
+                    files[i] = space + Path.GetFileName(thisParent.FullName);
                     parentPath = thisParent;
 
                     space += "    ";
                 }
 
-                Console.WriteLine(space + Path.GetFileName(entries[i]));
+                files[i] = space + Path.GetFileName(entries[i]);
             }
-            Console.ReadLine();
+
+            File.WriteAllLines("CycleTree.txt", files);
         }
+
+        #endregion
+
+        #region Рекурсия
+
+        private static void Recursive(string directory)
+        {
+            string[] dir = Directory.GetDirectories(directory);
+        }
+
+        #endregion
     }
 }
