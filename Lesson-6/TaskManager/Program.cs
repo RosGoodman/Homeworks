bd6543813e4c179fc9927276f21c2dab89d9a452
Ros <pavel.official@bk.ru>
2021-02-04 17:57:13 +0300

    готово, не проверено.

--- Lesson-6/TaskManager/Program.cs
+++ Lesson-6/TaskManager/Program.cs
@@ -1,23 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TaskManager
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            Console.Clear();
+            WriteAllProcess();
+            WriteAllCommands();
+            CommandsStarter();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>Вывод списка всех процессов.</summary>
+        private static void WriteAllProcess()
         {
             Process[] processes = Process.GetProcesses();
 
+            Console.WriteLine("          ID  Process Name");
+            Console.WriteLine("============  ================================");
+
             foreach (Process p in processes)
             {
-                Console.WriteLine("     {0}      {1}",p.Id ,p.ProcessName);
+                if (Console.ForegroundColor == ConsoleColor.White)
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                    Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("  {0,10}  {1,0}", p.Id, p.ProcessName);
             }
-            Console.ReadLine();
+        }
+
+        /// <summary>Вывод списка всех команд.</summary>
+        private static void WriteAllCommands()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine("\\res           - обновить; \n" +
+                "\\ki 1111       - остановить процесс по ID; \n" +
+                "\\kn FireFox    - остановить процесс по имени; \n" +
+                "\\c             - вывести список команд; \n" +
+                "\\q             - выход.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>Запуск введенной команды.</summary>
+        private static void CommandsStarter()
+        {
+            string commandStr = string.Empty;
+            do
+            {
+                commandStr = Console.ReadLine();
+            } while (commandStr == "");
+            
+            string[] command = commandStr.Split(' ');
+            switch (command[0])
+            {
+                case "\\res":
+                    Main();
+                    break;
+                case "\\ki":
+                    KillProcByID(command[1]);
+                    break;
+                case "\\kn":
+                    KillProcByName(command[1]);
+                    break;
+                case "\\c":
+                    WriteAllCommands();
+                    break;
+                case "\\q":
+                    break;
+                default:
+                    CommandsStarter();
+                    break;
+            }
+        }
+
+        /// <summary>Завершить процесс по ID.</summary>
+        /// <param name="processID">ID процесса.</param>
+        private static void KillProcByID(string processID)
+        {
+            foreach(Process process in Process.GetProcesses())
+            {
+                if (process.Id == Convert.ToInt32(processID)) process.Kill();
+            }
+        }
+
+        /// <summary>Завершить процесс по имени.</summary>
+        /// <param name="processID">Имя процесса.</param>
+        private static void KillProcByName(string processName)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName == processName) process.Kill();
+            }
         }
     }
 }
