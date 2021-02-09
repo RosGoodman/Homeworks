db91999d47a870058756ae4541f9b972d2d04ccd
Ros Goodman <pavel.official@bk.ru>
2021-02-09 14:12:40 +0300

    add les7 task1

---
+++ Lesson-7/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int c = 2;
+            decimal result = Calc(a, b, c);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static decimal Calc(int a, int b, int c)
+        {
+            decimal result = a + b;
+            result /= 2;
+            return result;
+        }
+    }
+}
