e364c5cb5f7ff90cca950732396e692640fb4458
Ros <pavel.official@bk.ru>
2021-02-12 18:23:40 +0300

    разбор вариантов сохранения настроек

--- Lesson-8/Task_1/Program.cs
+++ Lesson-8/Task_1/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 
 namespace Task_1
 {
@@ -6,7 +11,35 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Startup();
+        }
+
+        public static void Startup()
+        {
+            // строитель конфигурации
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                {"firstname", "Tom"},
+                {"age", "31"}
+                });
+            // создаем конфигурацию
+            AppConfiguration = builder.Build();
+
+        }
+        // свойство, которое будет хранить конфигурацию
+        public static IConfiguration AppConfiguration { get; set; }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public static void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(AppConfiguration["firstname"]);
+            });
         }
     }
 }
---
+++ Lesson-8/Task_1/Serices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal static class Serices
+    {
+        internal static void SaveSettings(GreetingSettings greetingSettings)
+        {
+
+        }
+
+        internal static GreetingSettings ReadSettings()
+        {
+            GreetingSettings greetingSettings = new GreetingSettings();
+            return greetingSettings;
+        }
+    }
+}
